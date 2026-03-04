f3d758b84640117cdd8bf3f06e8a296c24aed5b1
Nico Becker <nicolas1998.becker@gmail.com>
2026-03-04 12:33:54 +0100

    commit all other changes that were necessary for this networks including layers with custom layers for conv and transConv and transformation for mnist dataset

--- architectures/layers.py
+++ architectures/layers.py
@@ -17,24 +17,30 @@ class ConvLayer(nn.Module):
         super().__init__()
 
         self.model = nn.Sequential(
-            nn.Conv2d(in_channels=input_dim,out_channels=output_dim,kernel_size=kernel_size,stride=stride,padding=padding),
+            nn.Conv2d(in_channels=input_dim,out_channels=output_dim,kernel_size=kernel_size,stride=stride,padding=padding,bias=False),
             nn.BatchNorm2d(output_dim),
-            nn.LeakyReLU(0.2)
+            nn.ReLU(True)
         )
+        
         
         
     def forward(self,x):
         return self.model(x)
 
 class ConvTransposeLayer(nn.Module):
-    def __init__(self,input_dim,output_dim,kernel_size,stride,padding):
+    def __init__(self,input_dim,output_dim,kernel_size,stride,padding,last_layer:bool=True):
         super().__init__()
 
-        self.model = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=input_dim,out_channels=output_dim,kernel_size=kernel_size,stride=stride,padding=padding),
+        self.model = [
+            nn.ConvTranspose2d(in_channels=input_dim,out_channels=output_dim,kernel_size=kernel_size,stride=stride,padding=padding,bias=False),
             nn.BatchNorm2d(output_dim),
-            nn.ReLU(True)
-        )
+        ]
+        if last_layer:
+            self.model.append(nn.Tanh())
+        else:
+            self.model.append(nn.LeakyReLU(0.2))
+            
+        self.model = nn.Sequential(*self.model)
         
         
     def forward(self,x):
--- data/mnist.py
+++ data/mnist.py
@@ -1,7 +1,11 @@
 from torchvision.datasets import MNIST
-import torchvision.transforms as transform
+import torchvision.transforms as transforms
 
-transforms = transform.Compose([transform.ToTensor()])
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),      
+    transforms.ToTensor(),             
+    transforms.Normalize((0.5,), (0.5,))  
+])
 
 
 train_data = MNIST(r"C:\data_sets",train=True,download=True,transform=transforms)
